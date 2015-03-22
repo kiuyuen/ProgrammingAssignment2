81f4d42b60dbdb436b4dd07aac59487fa4c8fa1d
Kiu Yuen <yuenx040@umn.edu>
2015-03-22 15:25:46 -0500

    fisrt time submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R-Programming Assignment 2 (Date: 3/22/2015)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <<- function(x) {
+        
+        det_matrix <<- det(x)
+        
+        minor <<- function (x,i,j){
+              det_minor <- det(x[-i,-j])
+              }
+      
+        get_cofactor <- function(x,i,j){
+              ans <- (-1)^(i+j) * minor(x,i,j)
+              }
+                  
+        get_adjoint_matrix <- function(x){
+              n <<- nrow(x)
+              adjoint_matrix <<- matrix(NA,n,n)  
+                        for(i in 1:n){
+                        for(j in 1:n){
+                        adjoint_matrix[j,i] <- get_cofactor(x,i,j)
+                        }}
+              inversed_matrix <<- adjoint_matrix / det_matrix           
+              }
+        get_adjoint_matrix(x)
 
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+                
+                output <- makeCacheMatrix(x)    
+                if(!is.null(output)){
+                        message("you got it!  Good Job :D")
+                        return(output)
+                }
+
 }
