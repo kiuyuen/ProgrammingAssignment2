d9515223db5552f89a7b4e4262c21faebfa56092
Kiu Yuen <yuenx040@umn.edu>
2015-03-22 16:00:41 -0500

    second time submission, added cal for 2X2 matrices

--- cachematrix.R
+++ cachematrix.R
@@ -27,11 +27,32 @@ makeCacheMatrix <<- function(x) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+        if(det(x)==0){
+                message("Please use an invertible matrix!")
+                break
+        }
+        
+        if(nrow(x)==2){
+       
+                det <- det(x)
+                a11 <- x[1,1] 
+                a12 <- x[1,2] 
+                a21 <- x[2,1] 
+                a22 <- x[2,2] 
                 
+                x[1,1] = a22 / det
+                x[1,2] = -a12 / det
+                x[2,1] = -a21 / det
+                x[2,2] = a11 / det 
+                
+                print(x)
+        } else{
                 output <- makeCacheMatrix(x)    
                 if(!is.null(output)){
-                        message("you got it!  Good Job :D")
+                        message("You got it!  Good Job :D")
                         return(output)
+                        }
                 }
 
 }
